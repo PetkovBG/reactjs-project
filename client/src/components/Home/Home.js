b29d6ed861e2197bab9b8daaabd52fdf6e55d800
PetkovBG <97334281+PetkovBG@users.noreply.github.com>
2023-03-18 22:22:59 +0200

    Updated styling for the Home component.

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,13 +1,15 @@
 import styles from './Home.module.css';
+import { Link } from 'react-router-dom';
 
 export const Home = () => {
 
     return (
         <div className={styles['home-page']}>
-        <div className="background-image">
-          <h1>Welcome to Our Real Estate Website</h1>
-          <p>Find your dream home today!</p>
-          <button>Search Homes</button>
+        <div className={styles["background-image"]}>
+          <h1 className={styles.heading}>Petkov Real Estate</h1>
+          <h2 className={styles.subheading}>The Real Website for your dream home</h2>
+          <p className={styles.info}>Find your dream home today!</p>
+          <Link to="/catalog" className={styles.button}>Search Homes</Link>
         </div>
       </div>
     );
