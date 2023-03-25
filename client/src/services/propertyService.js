5d0b3d498fd662001ef158b1134c194a269b539d
PetkovBG <97334281+PetkovBG@users.noreply.github.com>
2023-03-25 10:53:09 +0200

    Update propertyService to propertyServiceFactory

--- client/src/services/propertyService.js
+++ client/src/services/propertyService.js
@@ -1,21 +1,35 @@
-import * as request from './requester';
+import { requestFactory } from './requester';
 
-const baseUrl = 'http://localhost:3030/jsonstore/properties';
+const baseUrl = 'http://localhost:3030/data/properties';
 
-export const getAll = async () => {
-    const result = await request.get(baseUrl);
 
-    const properties = Object.values(result);
+const propertyServiceFactory = (token) => {
 
-   return properties;
+    const request = requestFactory(token);
 
+    const getAll = async () => {
+        const result = await request.get(baseUrl);
+
+        const properties = Object.values(result);
+
+        return properties;
+
+
+    }
+
+    const getOne = async (propertyId) => {
+
+        const result = await request.get(`${baseUrl}/${propertyId}`);
+
+        return result;
+
+    };
+
+    return {
+        getAll,
+        getOne,
+        
+    }
 
 }
 
-export const getOne = async (propertyId) => {
-
-    const result = await request.get(`${baseUrl}/${propertyId}`);
-
-    return result;
-
-}
